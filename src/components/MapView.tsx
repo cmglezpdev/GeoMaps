dbd68625e7ab7cc5410c7aad1d8a35a8970848d2
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-07-20 19:49:17 -0400

    show map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,10 +1,28 @@
-import { useContext } from "react"
+import { useContext, useRef, useLayoutEffect } from 'react';
+import { Map } from 'maplibre-gl'; // or "const maplibregl = require('maplibre-gl');"
 import { PlacesContext } from '../context/index';
 import { Loading } from './index';
 
 export const MapView = () => {
   
-  const { isLoading, userLocation } = useContext(PlacesContext)
+  const { isLoading, userLocation } = useContext(PlacesContext);
+
+  const mapDiv = useRef<HTMLDivElement>(null);
+  
+  useLayoutEffect(() => {
+    
+    if( !isLoading ) {
+        const key = 'N5vFhyrwupGWYmu0WW9J';
+        const map = new Map({
+          container: mapDiv.current!, // container id
+          style: `https://api.maptiler.com/maps/streets/style.json?key=${key}`, // style URL
+            center: userLocation, // starting position [lng, lat]
+            zoom: 14 // starting zoom
+        });
+    }
+
+  }, [ isLoading ])
+  
   
   if (isLoading) {
     return (
@@ -13,7 +31,15 @@ export const MapView = () => {
   }
 
   return (
-    <div>
+    <div ref={ mapDiv }
+      style={{
+        height: '100vh',
+        width: '100vw',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+      }}
+    >
       { userLocation?.join(', ') }
     </div>
   )
